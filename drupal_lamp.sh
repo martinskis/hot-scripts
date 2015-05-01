915b62209c57cdb5c3b993f0fd6284ab64b5f460
martinskis <ljutvi@gmail.com>
2015-05-01 02:27:30 +0200

    Update drupal_lamp.sh

--- drupal_lamp.sh
+++ drupal_lamp.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-DATABASE_PASS=blueblue
-DRUPAL_DB_USER_PASS=redred
-DRUPAL_DB_USER=drupaldbuser
-DRUPAL_DB=drupaldb
-DRUPAL_USER=drupaluser
-DRUPAL_PASS=greengreen
-SITE_NAME=example
+source <(curl -s http://169.254.169.254/2009-04-04/user-data)
 
 function drush_install() {
 cd /var/www/html/
@@ -28,6 +22,10 @@ sed -ie '338s/AllowOverride None/AllowOverride All/' /etc/httpd/conf/httpd.conf
 service httpd start
 service mysqld start
 
+# properties must be without special characters
+
+
+
 mysqladmin -u root password "$DATABASE_PASS"
 mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
 mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
@@ -41,6 +39,7 @@ mysqladmin -u root -p"$DATABASE_PASS" create $DRUPAL_DB
 mysql -u root -p"$DATABASE_PASS" -e "CREATE USER '$DRUPAL_DB_USER'@'localhost' IDENTIFIED BY '$DRUPAL_DB_USER_PASS';"
 mysql -u root -p"$DATABASE_PASS" -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, LOCK TABLES, CREATE TEMPORARY TABLES ON $DRUPAL_DB.* TO '$DRUPAL_DB_USER'@'localhost' IDENTIFIED BY '$DRUPAL_DB_USER_PASS';"
 mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
+
 
 drush_install
 
